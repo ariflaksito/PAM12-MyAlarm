2dbb72dca96ac419358cafc86d9f7f2d144ef70a
Arif Laksito <arif.laksito@gmail.com>
2019-01-01 14:55:35 +0700

    add code

---
+++ app/src/main/java/net/ariflaksito/myalarm/AlarmPreference.java
@@ -0,0 +1,77 @@
+package net.ariflaksito.myalarm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AlarmPreference {
+    private final String PREF_NAME = "AlarmPreference";
+    private final String KEY_ONE_TIME_DATE = "oneTimDate";
+    private final String KEY_ONE_TIME_TIME = "oneTimeTime";
+    private final String KEY_ONE_TIME_MESSAGE = "oneTimeMessage";
+    private final String KEY_REPEATING_TIME = "repeatingTime";
+    private final String KEY_REPEATING_MESSAGE = "repeatingMessage";
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public AlarmPreference(Context context){
+        mSharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void setOneTimeDate(String date){
+        editor = mSharedPreferences.edit();
+        editor.putString(KEY_ONE_TIME_DATE, date);
+        editor.commit();
+    }
+
+    public String getOneTimeDate(){
+        return mSharedPreferences.getString(KEY_ONE_TIME_DATE, null);
+    }
+
+    public void setOneTimeTime(String time){
+        editor = mSharedPreferences.edit();
+        editor.putString(KEY_ONE_TIME_TIME, time);
+        editor.commit();
+    }
+
+    public String getOneTimeTime(){
+        return mSharedPreferences.getString(KEY_ONE_TIME_TIME, null);
+    }
+
+    public void setOneTimeMessage(String message){
+        editor = mSharedPreferences.edit();
+
+        editor.putString(KEY_ONE_TIME_MESSAGE, message);
+        editor.commit();
+    }
+
+    public String getOneTimeMessage(){
+        return mSharedPreferences.getString(KEY_ONE_TIME_MESSAGE, null);
+    }
+
+    public void setRepeatingTime(String time){
+        editor = mSharedPreferences.edit();
+        editor.putString(KEY_REPEATING_TIME, time);
+        editor.commit();
+    }
+
+    public String getRepeatingTime(){
+        return mSharedPreferences.getString(KEY_REPEATING_TIME, null);
+    }
+
+    public void setRepeatingMessage(String message){
+        editor = mSharedPreferences.edit();
+        editor.putString(KEY_REPEATING_MESSAGE, message);
+        editor.commit();
+    }
+
+    public String getRepeatingMessage(){
+        return mSharedPreferences.getString(KEY_REPEATING_MESSAGE, null);
+    }
+
+    public void clear(){
+        editor = mSharedPreferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+}
---
+++ app/src/main/java/net/ariflaksito/myalarm/NotificationUtils.java
@@ -0,0 +1,44 @@
+package net.ariflaksito.myalarm;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+public class NotificationUtils extends ContextWrapper {
+    private NotificationManager mManager;
+    public static final String ANDROID_CHANNEL_ID = "net.ariflaksito.myalarmmanager.ANDROID";
+    public static final String ANDROID_CHANNEL_NAME = "ANDROID CHANNEL";
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public NotificationUtils(Context base) {
+        super(base);
+        createChannels();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void createChannels() {
+
+        NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID, ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+
+        androidChannel.enableLights(true);
+        androidChannel.enableVibration(true);
+        androidChannel.setLightColor(Color.GREEN);
+        androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(androidChannel);
+
+
+    }
+
+    public NotificationManager getManager() {
+        if (mManager == null) {
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return mManager;
+    }
+}
